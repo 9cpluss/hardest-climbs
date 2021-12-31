914af31a74b59f2eb133b8ab2e7b2161db6b1a7e
Dev <developer@9cplus.com>
2021-12-31 19:13:20 +0000

    Move data files to data folder

--- flask_app.py
+++ flask_app.py
@@ -8,12 +8,12 @@ from functions import create_html_columns, create_html_from_json_element
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
 
-app = Flask("9c plus", template_folder='/home/9cpluss/hardest-climbs')
+app = Flask("Hardest Climbs", template_folder='/home/9cpluss/mysite')
 
-with open(os.path.join(THIS_FOLDER, 'lead.json'), "r") as f:
+with open(os.path.join(THIS_FOLDER, 'data/lead.json'), "r") as f:
     lead_data = json.load(f)
 
-with open(os.path.join(THIS_FOLDER, 'boulder.json'), "r") as f:
+with open(os.path.join(THIS_FOLDER, 'data/boulder.json'), "r") as f:
     boulder_data = json.load(f)
 
 
