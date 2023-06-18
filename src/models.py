6788f0fb72289b913e5ae6d7aacfc774ebfc104a
9c Pluss <96419872+9cpluss@users.noreply.github.com>
2023-06-18 17:06:50 +0200

    Add more models

--- src/models.py
+++ src/models.py
@@ -16,3 +16,34 @@ class Grades(db.Model):
     french = db.Column(db.String(15))
     us = db.Column(db.String(15))
     style = db.Column(db.String(31))
+
+
+class Locations(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    country = db.Column(db.String(255))
+    area = db.Column(db.String(255))
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+
+
+class Climbs(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    style = db.Column(db.String(31))
+    fa_id = db.Column(db.Integer, db.ForeignKey("climbers.id"), nullable=False)
+    fa_date = db.Column(db.String(15))
+    grade_id = db.Column(db.Integer, db.ForeignKey("grades.id"), nullable=False)
+    location_id = db.Column(db.Integer, db.ForeignKey("locations.id"), nullable=False)
+
+
+class Repeats(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    climb_id = db.Column(db.Integer, db.ForeignKey("climbs.id"), nullable=False)
+    climber_id = db.Column(db.Integer, db.ForeignKey("climbers.id"), nullable=False)
+
+
+class Videos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    climb_id = db.Column(db.Integer, db.ForeignKey("climbs.id"), nullable=False)
+    climber_id = db.Column(db.Integer, db.ForeignKey("climbers.id"), nullable=False)
+    url = db.Column(db.String(255))
