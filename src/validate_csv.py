177b061b3bf8bfbb98bd77d58e46bde15da1144f
9cpluss <hardestclimbs@proton.me>
2025-01-26 15:55:50 +0100

    Formatting of CSV validation

---
+++ src/validate_csv.py
@@ -0,0 +1,62 @@
+import pandera as pa
+import pandas as pd
+from datetime import datetime
+
+
+current_year = datetime.now().year
+
+
+climbers_schema = pa.DataFrameSchema(
+    {
+        'climber_id': pa.Column(
+            dtype=str,
+            checks=pa.Check(lambda s: not s.duplicated().any()),
+            unique=True
+        ),
+        'gender': pa.Column(str, pa.Check.isin(['male', 'female'])),
+        'year_of_birth':  pa.Column(
+            dtype=float,
+            checks=pa.Check(lambda x: (pd.isna(x)) | ((x >= 1970) & (x <= current_year - 15))),
+            nullable=True
+        )
+    }
+)
+
+
+routes_schema = pa.DataFrameSchema(
+    {
+        'route_id': pa.Column(
+            dtype=str,
+            checks=pa.Check(lambda s: not s.duplicated().any()),
+            unique=True
+        ),
+        'type': pa.Column(str, pa.Check.isin(['sport', 'boulder']))
+    }
+)
+
+
+sends_schema = pa.DataFrameSchema(
+    {
+        'date': pa.Column(
+            dtype=object,
+            nullable=True
+        )
+    }
+)
+
+
+def validate_csv(file_path: str, schema: pa.DataFrameSchema):
+    df = pd.read_csv(file_path)
+
+    try:
+        schema.validate(df, lazy=True)
+        print("Data validation successful!")
+    except pa.errors.SchemaErrors as err:
+        print("Data validation failed:")
+        print(err.failure_cases)
+
+
+if __name__ == "__main__":
+    validate_csv(file_path='data/climbers_table.csv', schema=climbers_schema)
+    validate_csv(file_path='data/routes_table.csv', schema=routes_schema)
+    validate_csv(file_path='data/sends_table.csv', schema=sends_schema)
